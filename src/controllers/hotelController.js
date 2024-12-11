6c5cc0e13781b67beeaffb9d5fa6ca9a43df59bd
m_hasan <mhasan@neuvays.com>
2024-12-12 03:50:56 +0530

    Changes

--- src/controllers/hotelController.js
+++ src/controllers/hotelController.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const axios = require("axios");
+const Hotel = require('../models/hotelModel');
+const sendEmail = require('../config/mailer');
+const sendSMS = require("../config/sms_sender"); 
+
+function generateReferenceNumber() {
+  const timestamp = Date.now();
+  const randomSuffix = Math.floor(Math.random() * 10000);
+  return `REF${timestamp}${randomSuffix}`;
+}
 
 module.exports = function (app) {
   const apiRoutes = express.Router();
@@ -36,6 +45,72 @@ module.exports = function (app) {
       });
     }
   });
+
+  apiRoutes.post('/bookMyHotel', async (req, res) => {
+    const { email, contact, checkIn, checkOut, travellers, hotelPrice, numberOfDays } = req.body;
+    try {
+      const referenceNo = generateReferenceNumber()
+      const rent = Number(numberOfDays * hotelPrice);
+      const booking = new Hotel({ email, contact, checkIn, checkOut, travellers, referenceNo, hotelPrice, rent });
+      await booking.save();
+
+      const userSubject = `Hotel Booking Confirmation`;
+            const ownerSubject = `New Hotel Booking Received`;
+            
+            const userText = `
+                Dear Traveller,
+
+                Thank you for booking a Hotel with us. Here are your booking details:
+
+                Check In Date: ${checkIn}
+                Check Out Date: ${checkOut}
+                Guests: ${travellers}
+                Estimated Rent (May Vary on arrival): ${rent}
+                Reference Number: ${referenceNo}
+
+                We will contact you shortly to confirm your booking.
+
+                Regards,
+                Tuba International
+            `;
+
+            const ownerText = `
+                Hello,
+
+                A new Hotel Booking has been received. Here are the details:
+
+                Check In Date: ${checkIn}
+                Check Out Date: ${checkOut}
+                Guests: ${travellers}
+                Estimated Rent (May Vary on arrival): ${rent}
+                Reference Number: ${referenceNo}
+
+
+                Please review and confirm the booking.
+
+                Regards,
+                Tuba International
+            `;
+
+            await sendEmail(email, userSubject, userText); // Email to the user
+            await sendEmail(process.env.OWNER_MAIL, ownerSubject, ownerText); // Email to the owner
+            const ownerPhone = "8948626497"; // Replace with the owner's phone number
+            const message = `New Hotel Booking Received`;
+            await sendSMS(ownerPhone, message);
+
+      res.status(200).send({
+        msg: 'Hotel Booked successfully: We will get back to you soon!',
+        bookingData: booking
+      });
+    }
+
+    catch (error) {
+
+      console.error('Error while storing data:', error);
+      res.status(500).send({ msg: 'Error while storing data.', error: error.message });
+    }
+
+  })
 
   app.use("/", apiRoutes);
 };
---
+++ src/models/hotelModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const hotelSchema = new mongoose.Schema(
+    {                                      
+        email: {
+            type: String,
+            required: false,
+            unique: false,  
+        },
+        contact: {
+            type: String,
+            required: true
+        },
+        checkIn: {
+            type: String,
+            required: false,
+            unique: false,  
+        },
+        checkOut: {
+            type: String,
+            required: false
+        },
+        travellers: {
+            type: Array,
+            required: false
+        },
+        referenceNo: {
+            type: String,
+        },
+
+        rent: {
+            type: String,
+        }
+     
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Hotel', hotelSchema);
