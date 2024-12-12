ddb778030441d495e452ac8ea6e4784c1cb92219
mhasanmzp <m.hasanmzp@gmail.com>
2024-12-12 23:50:40 +0530

    Changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,6 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
 const cors = require("cors");
-
-const saltRounds = 10;
-
-async function hashPassword(password) {
-    return bcrypt.hash(password, saltRounds);
-}
 
 module.exports = function (app) {
     const apiRoutes = express.Router();
@@ -18,11 +11,9 @@ module.exports = function (app) {
             const existingUser = await User.findOne({ email });
             if (existingUser) {
                 return res.status(400).send({ msg: 'User already registered.' });
-            }                                
+            }
 
-            const hashedPassword = await hashPassword(password);
-            const newUser = await User.create({ name, email, password: hashedPassword });
-
+            const newUser = await User.create({ name, email, password });
             res.status(201).send({ msg: 'User registered successfully.', user: newUser });
         } catch (err) {
             res.status(500).send({ msg: 'Error registering user.', error: err.message });
@@ -33,30 +24,12 @@ module.exports = function (app) {
         const { email, password } = req.body;
         try {
             const user = await User.findOne({ email });
-            if (!user || !(await bcrypt.compare(password, user.password))) {
+            if (!user || user.password !== password) {
                 return res.status(400).send({ msg: 'Invalid credentials.' });
             }
             res.status(200).send({ msg: 'Login successful.', user });
         } catch (err) {
             res.status(500).send({ msg: 'Error logging in.', error: err.message });
-        }
-    });
-
-    apiRoutes.post('/storeName', async (req, res) => {
-        const { name } = req.body;
-
-        if (!name) {
-            return res.status(400).send({ msg: 'Name is required.' });
-        }
-
-        try {
-            const newUser = new User({ name });
-            await newUser.save();
-
-            res.status(201).send({ msg: 'User created successfully.', user: newUser });
-        } catch (err) {
-            console.error('Error while storing data:', err);
-            res.status(500).send({ msg: 'Error while storing data.', error: err.message });
         }
     });
 
