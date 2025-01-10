81973d6c971727bc30c5723f6d64b6a586e6d49b
m_hasan <m.hasanmzp@gmail.com>
2025-01-11 00:38:09 +0530

    Changes

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
             type: String, 
             default: 'user'
          },
+         phone: { 
+            type: String, 
+            required: false 
+        },   
     },
     { timestamps: true }
 );
